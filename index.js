6eb8cbc7d11f24e23897768798298cbfae2174af
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2023-12-21 04:01:38 -0300

    fixeandoparasegundapreentrega

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ const funcionFlecha = (p1,p2,p3)=>{
     console.log('Hola')
 }
 ----------------------------------------
-*/
-
+------primer preentrega------
 function calcularnotas (nombre, nota1, nota2, nota3){
     let nombrefix = nombre.trim();
     let operacion = nota1*0.35+nota2*0.20+nota3*0.45;
@@ -65,6 +64,167 @@ function calcularnotas (nombre, nota1, nota2, nota3){
 
 alert('Calculador de notas promedio');
 calcularnotas(prompt('Nombre del alumno'),prompt('Del 1 al 10, cual es la nota en Parciales'),prompt('Del 1 al 10, cual es la nota en Proyectos'),prompt('Del 1 al 10, cual es la nota en Examenes'));
+
+--------------------
+class bolsa{ 
+    constructor(nombre,apellido,edad){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.edad = edad
+    }
+    hablar(mensaje){
+        console.log(mensaje+'HOLA'+this.nombre)
+    }
+}
+
+
+let consorcio = new bolsa('Jorge','Mamamani',20);
+
+
+consorcio.hablar('Hola');
+*/
+
+/*
+const persona1 = {
+    nombre: 'Ronan',
+    apellido: 'Pendragon'
+}
+
+const persona2 = {
+    nombre: 'Gorgos',
+    apellido: 'Stonehoof'
+}
+
+const persona3 = {
+    nombre: 'Zegga',
+    apellido: 'Windwalk',
+    ataque: 'bola de energía'
+}
+
+const personas = [persona1, persona2, persona3]
+const sucesion = ['Un patito', 'Dos patitos', 'Tres patitos', 'Cuatro patitos']
+
+/*
+for(let i=0; i<personas.length; i++){
+    console.log(personas[i])
+}
+*/
+
+/*
+let whilecontador = 0
+
+while(whilecontador<=10){
+    console.log(whilecontador)
+    whilecontador++;
+}
+
+
+let dowhilecontador = 0
+console.log('dowhilecontador está en 0')
+
+do{
+    dowhilecontador++;
+    console.log('dowhilecontador terminó DO y quedó en 1');
+}
+while(dowhilecontador==1){
+    console.log('dowhilecontador entró al while y se le sumó otro');
+    dowhilecontador++;
+    console.log('El valor actual del contador es '+dowhilecontador);
+}
+*/
+
+const profesor = {
+    nombre: 'Armando Esteban',
+    apellido: 'Quito',
+}
+const arrayej = [
+    {
+        nombre: 'Fulano',
+        nota: 8,
+    },
+    {
+        nombre: 'Mengano',
+        nota: 5,
+    },
+    {
+        nombre: 'Sultano',
+        nota: 7,
+    }
+]
+
+function validarNombre(nombre) {
+    expresion = /^[A-Za-z]+(?:\s[A-Za-z]+)*$/;
+    if(expresion.test(nombre)) {
+      return true; 
+    } else {
+      return false; 
+    }
+}
+
+let firstchoise = prompt('Obtener el promedio de notas? \n Si \n No').toLowerCase();
+
+switch (firstchoise) {
+    case 'si':
+        choise = firstchoise;
+        while(choise == 'si' ){
+            switch(choise){
+                case 'si': 
+                const calculador = function(nombre, nota1, nota2, nota3){
+                    let nombrefix = nombre.trim(); 
+                    let operacion = nota1*0.35+nota2*0.20+nota3*0.45;
+                    
+                    if (!isNaN(nota1.trim()) && nota1.trim() !== "" && nota2.trim() && nota2.trim() !== "" && nota3.trim() && nota3.trim() !== "" && nota1>=0 && nota1<=10 && nota2>=0 && nota2<=10 && nota3>=0 && nota3<=10 && validarNombre(nombrefix) && nombrefix!=null){
+                        //Validación CORRACTA
+                        arrayej.push({nombre: nombre, nota: operacion});
+                        console.log('La nota promedio de '+nombrefix+' es de: '+operacion);
+                    
+                        let general = prompt('Desea ver la lista completa de notas de alumnos? \n Aceptar \n Cancelar').toLowerCase();
+                        switch(general){
+                            case 'aceptar':
+                                console.log('Las notas generales son:')
+                                for(let i=0;i<arrayej.length;i++){
+                                console.log('Nota final de '+arrayej[i].nombre+': '+arrayej[i].nota)
+                                }
+                                console.log('A cargo del Profesor: '+profesor.nombre+' '+profesor.apellido);
+                            break;
+                            case 'cancelar':
+                            break;
+                            default:
+                                console.log('Error, opción de elección de notas finales inválida, por favor eliga entre Aceptar o Cancelar');
+                            break;
+                            }
+                        } else {
+                        //ERROR
+                        console.log("Error, algún valor ha sido colocado erroneamente. Tenga en cuenta las siguientes pautas: \n 1-El nombre no debe ser ni números ni caracteres especiales ni espacios en blanco \n 2-Las notas deben ser números, entre el 1 y 10, no puede ser ni más ni menos ni tampoco letras, o espacios en blanco \n 3-No dar en 'Aceptar' sin haber colocado un valor previamente");
+                    }
+                }
+                //LLAMADA
+                calculador(prompt('Dame el nombre'), prompt('Dame la nota 1'), prompt('Dame la nota 2'), prompt('Dame la nota 3'));
+                break;
+
+                case 'no':
+                    console.log('while/switch/No')
+                break;
+
+                default:
+                    console.log('while/switch/Default')
+                break;
+            }
+            choise = prompt('Desea realizar un NUEVO calculo? \n Si \n No').toLowerCase();
+        }
+        
+    break;
+    case 'no':
+        console.log('Usted ha declarado que NO desea calcular el Promedio, buen día')
+    break;
+    default:
+        console.log('Dato invalido')
+    break;
+}
+
+  
+
+
 
 
 
